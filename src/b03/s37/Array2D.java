ed06d77cb51ae42ee790cdee391871d4c6ce3b34
Antonio Pontrelli <ant.pontrelli@gmail.com>
2019-07-23 14:01:39 +0200

    modificalezione2

--- src/b03/s37/Array2D.java
+++ src/b03/s37/Array2D.java
@@ -3,9 +3,10 @@ package b03.s37;
 public class Array2D {
     public static void main(String[] args) {
         int[][] array2d = new int[4][5];
-
         int value = array2d[2][3];
-
+        array2d[2][3] = 7;
+        System.out.println(array2d[2][3]);
+        value=array2d[2][3]*3;
         System.out.println(array2d[2][3] + ", " + value);
         System.out.println(array2d[3][4]);
     }
