975cfb67e0b81c3489401a56a7c9c9af0ad4b5bf
Antonio Pontrelli <ant.pontrelli@gmail.com>
2019-07-23 14:55:44 +0200

    if_modificato_lezione2

--- src/b04/s38/If.java
+++ src/b04/s38/If.java
@@ -2,12 +2,23 @@ package b04.s38;
 
 public class If {
     public static void main(String[] args) {
-        boolean condition = true;
-        
-        if (condition) {
-            System.out.println("only if condition is true");
+        //boolean condition = true;
+    	//int a = 3;
+        int a = 12;
+        if (a < 0) {
+            System.out.println("negativo");
         }
-
-        System.out.println("always printed");
+        else if(a > 0 && a < 100) {
+        System.out.println("piccolo");
+        if(a % 2 == 0) {
+        	System.out.println("pari");
+        }
+        else {
+        	System.out.println("dispari");
+        	}       	
+        }
+        else {	
+        System.out.println("grande");
+        }
     }
 }
