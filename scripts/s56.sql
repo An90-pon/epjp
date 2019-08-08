b2d464fab60ecdf6558c4158ac0dc72ff141dc9e
Administrator <Administrator@192.168.12.55>
2019-08-08 18:01:38 +0200

    08/08/2019Mod

--- scripts/s56.sql
+++ scripts/s56.sql
@@ -1,3 +1,11 @@
 update regions
 set region_name = 'Region ' || region_id
 where region_id > 10;
+
+select *
+from regions;
+
+select *
+from regions
+where region_id > 10;
+
--- scripts/s57.sql
+++ scripts/s57.sql
@@ -1,2 +1,6 @@
 delete from regions
+where region_id > 10; --finchè non faccio commit i cambiamenti sono reversibii
+
+select * 
+from regions
 where region_id > 10;
